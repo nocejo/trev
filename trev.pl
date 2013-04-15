f4ee0456ffabf7206d2ad588cda236f0cd873ba0
nocejo <fidel.mato@gmail.com>
2013-04-15 20:05:27 +0200

    Switch to en-US L10N

--- trev.pl
+++ trev.pl
@@ -20,29 +20,29 @@ my $sepstyle = "underline bold";
 
 # Uncomment STRINGs in your preferred localization ---------------------------------- L10N
 # ---------------------------------------------------------------------------------- en-US
-#my $STRING_LBL_SEL  = "Selected:";
-#my $STRING_MSG_AMB  = "is ambiguous, can be:";
-#my $STRING_MSG_END  = "Finished.";
-#my $STRING_MSG_ERR  = "Warning: not completed";
-#my $STRING_MSG_NON  = "\nNone\n\n";
-#my $STRING_MSG_QIT  = "Terminated (task ";
-#my $STRING_MSG_RET  = "Press [RET] to continue: ";
-#my $STRING_MSG_STA  = ": doesn't appear as visible.";
-#my $STRING_MSG_UND  = "Not understood.";
-#my $STRING_MSG_VER  = "Taskwarrior version must be 2.2.0 at least.";
-#my $STRING_NOW_TXT  = "Now reviewing:";
+my $STRING_LBL_SEL  = "Selected:";
+my $STRING_MSG_AMB  = "is ambiguous, can be:";
+my $STRING_MSG_END  = "Finished.";
+my $STRING_MSG_ERR  = "Warning: not completed";
+my $STRING_MSG_NON  = "\nNone\n\n";
+my $STRING_MSG_QIT  = "Terminated (task ";
+my $STRING_MSG_RET  = "Press [RET] to continue: ";
+my $STRING_MSG_STA  = ": doesn't appear as visible.";
+my $STRING_MSG_UND  = "Not understood.";
+my $STRING_MSG_VER  = "Taskwarrior version must be 2.2.0 at least.";
+my $STRING_NOW_TXT  = "Now reviewing:";
 # ---------------------------------------------------------------------------------- es-ES
-my $STRING_LBL_SEL = "Seleccionadas:";
-my $STRING_MSG_AMB = "es ambiguo, puede ser:";
-my $STRING_MSG_END = "Finalizado.";
-my $STRING_MSG_ERR = "Aviso: no se completa";
-my $STRING_MSG_NON = "\nNinguna\n\n";
-my $STRING_MSG_QIT = "Terminado (tarea ";
-my $STRING_MSG_RET = "Presione [RET] para continuar: ";
-my $STRING_MSG_STA = ": no aparece como visible.";
-my $STRING_MSG_UND = "No comprendido.";
-my $STRING_MSG_VER = "Taskwarrior debe estar al menos en su versión 2.2.0 .";
-my $STRING_NOW_TXT = "Revisando ahora:";
+#my $STRING_LBL_SEL = "Seleccionadas:";
+#my $STRING_MSG_AMB = "es ambiguo, puede ser:";
+#my $STRING_MSG_END = "Finalizado.";
+#my $STRING_MSG_ERR = "Aviso: no se completa";
+#my $STRING_MSG_NON = "\nNinguna\n\n";
+#my $STRING_MSG_QIT = "Terminado (tarea ";
+#my $STRING_MSG_RET = "Presione [RET] para continuar: ";
+#my $STRING_MSG_STA = ": no aparece como visible.";
+#my $STRING_MSG_UND = "No comprendido.";
+#my $STRING_MSG_VER = "Taskwarrior debe estar al menos en su versión 2.2.0 .";
+#my $STRING_NOW_TXT = "Revisando ahora:";
 
 # ---------------------------------------------------------- selection and filter defaults
 my $selatt = "active";    # selection attribute
